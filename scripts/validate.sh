9e3c293fde47e5ecc50da57872d7f12c39e2c433
chjomoon <33528293+chjomoon@users.noreply.github.com>
2025-12-22 13:35:59 +0900

    disable validatae

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
-set -e
-sleep 10
-curl -fsS http://localhost:8080/health || exit 1
+##!/usr/bin/env bash
+#set -e
+#sleep 10
+#curl -fsS http://localhost:8080/health || exit 1
+
